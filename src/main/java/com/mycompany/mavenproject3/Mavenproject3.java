7ce6ef3c0596045e1c303e02a3f196e55970c8be
nquorishi <94141633+nquorishi@users.noreply.github.com>
2023-01-25 13:14:43 -0500

    second commit

--- src/main/java/com/mycompany/mavenproject3/Mavenproject3.java
+++ src/main/java/com/mycompany/mavenproject3/Mavenproject3.java
@@ -11,6 +11,6 @@ package com.mycompany.mavenproject3;
 public class Mavenproject3 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World... second commit!");
+        System.out.println("Hello World... second commit!!");
     }
 }
